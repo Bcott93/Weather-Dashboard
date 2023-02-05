2278234f517604d704e1856ac93029e6cc762ca3
Benjamin Cottrell <bcott93@gmail.com>
2023-02-05 15:26:50 +0000

    Applied DRY logic by adding functions, added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,141 +3,125 @@ $(document).ready
 
 // Creates a variable for the search button in HTML
 const searchBtn = $("#search-button")
-// My key to access the data from Openweathers API's
-// let apiKey = "a0822daa77d2fe015266567cae2f77a3"
-const tempKey = "d91f911bcf2c0f925fb6535547a5ddc9"
 
-// Adds a click event to the search button
+// My key to access the data from Openweathers API's
+const apiKey = "a0822daa77d2fe015266567cae2f77a3"
+
+// Creates a function to show userInput history
+function createHistoryBtn(cityFormatted) {
+  // Creates a variable to make and name a history button
+  const historyBtn = $("<button>").text(cityFormatted)
+  // Adds functionality to the button
+  historyBtn.on("click", function (event) {
+   // Stops the page refreshing
+   event.preventDefault()
+   // Clears the currently shown data on the HTML page
+   $("#weather-container").empty()
+   // Runs the weatherdata function for the relevant city to the button
+   getWeatherData(cityFormatted)
+ })
+ // Adds the button to the HTML document
+ $("#history").append(historyBtn)
+}
+
+// Adds a click event to the search button 
 $("#search-button").on("click", function (event) {
-  // Stops the page from refreshing upon input.
+  // Stops the page refreshing
   event.preventDefault()
   // Adds a variable for the userinput
   const city = $('#search-input').val().trim()
   // Formats the user input
-  const cityFormatted= city.charAt(0).toUpperCase() + city.slice(1).toLowerCase()
-  // Creates a variable to add the history to the left side of the document
-  const historyBtn = $("<button>")
-  // Adds a name to the created button
-  historyBtn.text(cityFormatted)
-  // Adds the button to the HTML document
-  $("#history").append(historyBtn)
+  const cityFormatted = city.charAt(0).toUpperCase() + city.slice(1).toLowerCase()
+  // Runs the create history button function
+  createHistoryBtn(cityFormatted)
+  // Runs the function to populate the on click function
+  getWeatherData(cityFormatted);
+}); 
 
-
+// Creates a function which finds the required weather data
+function getWeatherData(cityFormatted) {
   // Sets the URL for the location API, including the variables required
-  let geoQueryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + tempKey
+  let geoQueryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityFormatted + "&appid=" + apiKey
   // Uses Jquery to pull the data from the API
   $.ajax({
-    url: geoQueryURL,
-    method: "GET"
-  // Once the above function has taken place, sets variables and runs the second API query to find the weather
+      url: geoQueryURL,
+      method: "GET"
+    // Tells JS to wait until the above function has completed
   }).then(function (response) {
-    console.log(response);
-        // Finds the data within the API, and sets the Longitute and Latitude    
-        let latitude = response[0].lat.toFixed(2);
-        let longitude = response[0].lon.toFixed(2);
-        // Sets the URL for the location API, including the variables required
-        let weatherQueryURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + tempKey;
+    // Sets variables from first ajax to populate API query
+    let latitude = response[0].lat.toFixed(2)
+    let longitude = response[0].lon.toFixed(2)
+     // Sets the URL for the location API, including the variables required
+    let weatherQueryURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey
+  
+      // Uses Jquery to pull the data from the API
+      $.ajax({
+          url: weatherQueryURL,
+          method: "GET"
+      // Tells JS to wait until the above function has completed
+      }).then(function (response) {
+        // Sets the Variable responseData to an array
+        let responseData = []
+        // Loops the response from the api, and pulls every 8th input. 8th input to ensure data is at a consistant time of day
+        for(let i = 0; i < response.list.length; i+=8) {
+        // Sets the date and formats it  
+        let responseDate = moment(response.list[i].dt_txt).format("Do MMMM YYYY")
+        // Creates a div with the temperature value, changes Kelvin to Celsius and shows the Celsius symbol
+        let responseTemp = $("<div>").append("Temp: ", ((response.list[i].main.temp - 273.15).toFixed(2)  +'&#8451;'))
+        // Creates a variable with the code indicating the current weather
+        let responseIcon = (response.list[i].weather[0].icon)
+        // Pulls the corresponding icon from the URL
+        let responseIconShow = "https://openweathermap.org/img/w/" + responseIcon + ".png"
+        // Creates a div with the humidity value, adds a % symbol
+        let responseHumidity = $("<div>").append("Humidity: ", (response.list[i].main.humidity) + '%')
+        // Creates a div with the Wind Speed variable and sets its unit of measure
+        let responseWindSpeed = $("<div>").append("Wind Speed: ", (response.list[i].wind.speed) + ' meters per second')
+        
+        // Pushes the response data into an array of objects for ease of access
+        responseData.push({
+        date: responseDate,
+        temp: responseTemp,
+        icon: responseIcon,
+        iconShow: responseIconShow,
+        humidity: responseHumidity,
+        windSpeed: responseWindSpeed
+      })
+      // If there is data, empty the container and run the loop
+      if (responseData.length > 0) {
+        $("#weather-container").empty()
+        // Loop the responseData
+        for (let i = 0; i < responseData.length; i++) {
+         // Create a div to input the data, added a class for formatting
+         let newDiv = $("<div>").addClass("response-section")
+         // Adds a header with the City name and the date
+         let cityName = $("<h2>").text(cityFormatted + " (" + responseData[i].date + ")")
+         // Adds the icon to the section
+         let weatherIcon = $("<img>").attr("src", responseData[i].iconShow)
+         // Sets variables for the rest of the data required
+         let temp = responseData[i].temp
+         let humidity = responseData[i].humidity
+         let windSpeed = responseData[i].windSpeed
+  
+        // Appends all the variables and adds them to the HTML
+        newDiv.append(cityName, weatherIcon, temp, humidity, windSpeed)
+        $("#weather-container").append(newDiv)
+      }
+     }
+    }
+  })
+ })
+}  
+        
+  
+    
 
-  // Uses Jquery to pull the data from the API
-        $.ajax({
-            url: weatherQueryURL,
-            method: "GET"
-        }).then(function (response) {
-          // Sets the Variable responseData to an array
-          let responseData = []
-          // Loops the Reponse data, and pulls every 8th input
-          for(let i = 0; i < response.list.length; i+=8) {
-          let responseDate = moment(response.list[i].dt_txt).format("Do MMMM YYYY")
-          let responseTemp = $("<div>").append("Temp: ", ((response.list[i].main.temp - 273.15).toFixed(2)  +'&#8451;'))
-          let responseIcon = (response.list[i].weather[0].icon)
-          let responseIconShow = "https://openweathermap.org/img/w/" + responseIcon + ".png"
-          let responseHumidity = $("<div>").append("Humidity: ", (response.list[i].main.humidity) + '%')
-          let responseWindSpeed = $("<div>").append("Wind Speed: ", (response.list[i].wind.speed) + ' meters per second')
+                
            
-          responseData.push({
-            date: responseDate,
-            temp: responseTemp,
-            icon: responseIcon,
-            iconShow: responseIconShow,
-            humidity: responseHumidity,
-            windSpeed: responseWindSpeed
-          })
-         
-          if (responseData.length > 0) {
-            $("#weather-container").empty()
-        
-            for (let i = 0; i < responseData.length; i++) {
-                let newSection = $("<div>").addClass("response-section")
-                let cityName = $("<h2>").text(cityFormatted + " (" + responseData[i].date + ")")
-                let weatherIcon = $("<img>").attr("src", responseData[i].iconShow)
-                let temp = responseData[i].temp
-                let humidity = responseData[i].humidity
-                let windSpeed = responseData[i].windSpeed
-
-
-                newSection.append(cityName, weatherIcon, temp, humidity, windSpeed)
-                $("#weather-container").append(newSection)
-
-                historyBtn.on("click", function (event) {
-                  event.preventDefault()
-                  $("#weather-container").empty()
-                  
-                  for(let i = 0; i < response.list.length; i+=8) {
-                    let responseDate = moment(response.list[i].dt_txt).format("Do MMMM YYYY")
-                    let responseTemp = $("<div>").append("Temp: ", ((response.list[i].main.temp - 273.15).toFixed(2)  +'&#8451;'))
-                    let responseIcon = (response.list[i].weather[0].icon)
-                    let responseIconShow = "https://openweathermap.org/img/w/" + responseIcon + ".png"
-                    let responseHumidity = $("<div>").append("Humidity: ", (response.list[i].main.humidity) + '%')
-                    let responseWindSpeed = $("<div>").append("Wind Speed: ", (response.list[i].wind.speed) + ' meters per second')
-                     
-                    responseData.push({
-                      date: responseDate,
-                      temp: responseTemp,
-                      icon: responseIcon,
-                      iconShow: responseIconShow,
-                      humidity: responseHumidity,
-                      windSpeed: responseWindSpeed
-                    })
-                   
-                    if (responseData.length > 0) {
-                      $("#weather-container").empty()
-                  
-                      for (let i = 0; i < 5; i++) {
-                          let newSection = $("<div>").addClass("response-section")
-                          let cityName = $("<h2>").text(cityFormatted + " (" + responseData[i].date + ")")
-                          let weatherIcon = $("<img>").attr("src", responseData[i].iconShow)
-                          let temp = responseData[i].temp
-                          let humidity = responseData[i].humidity
-                          let windSpeed = responseData[i].windSpeed
-          
-          
-                          newSection.append(cityName, weatherIcon, temp, humidity, windSpeed)
-                          $("#weather-container").append(newSection)
-                      }
-                    }
-                  }
-
-               
-                })
-                
-                
-            }
-        }
-        
-          }
-         
-         
-        });
+    
+      
 
      
-    });
-});
 
 
 
 
-
-  //  // Sets a variable for the time using moment() and formats
-  //  let currentDate = moment().format("Do MMMM YYYY")
-  //  $("#today").append(currentDate)
-  //  console.log(currentDate)
